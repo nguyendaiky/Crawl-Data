0471608e507f836c3b07e0d5981f04aaea1e5bec
Nguyễn Đại Kỳ <70926535+nguyendaiky@users.noreply.github.com>
2021-06-24 01:31:24 +0700

    upload new file

--- CrawlFireWatch/testdate.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-dateStart = datetime.datetime(2021,6,15)
-dateEnd = datetime.datetime(2021,1,1)
-while True:
-    print(dateStart.strftime("%d/%m/%Y")) 
-    if dateStart == dateEnd:
-        break 
-    dateStart -= datetime.timedelta(days=1)
